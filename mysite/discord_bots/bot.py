425ab6a70cfd31d3d4614c97a5f4c697d00b3981
bogdan <bogdankibuj@gmail.com>
2025-03-29 19:55:38 +0200

    create another script for bot, for add members which was on servers

--- mysite/discord_bots/bot.py
+++ mysite/discord_bots/bot.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))  # Додаємо каталог mysite у шлях
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")  # Вказуємо, де шукати налаштування Django
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 import django
 
---
+++ mysite/discord_bots/member_reader_bot.py
@@ -0,0 +1,48 @@
+import discord
+import sys
+import os
+from asgiref.sync import sync_to_async
+
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+import django
+django.setup()
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+from catalog.models import DiscordUser
+
+@sync_to_async
+def create_or_get_user(user_id, defaults):
+    return DiscordUser.objects.get_or_create(user_id=user_id, defaults=defaults)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    await fetch_members()
+
+async def fetch_members():
+    for guild in bot.guilds:
+        async for member in guild.fetch_members(limit=None):
+            user_id = member.id
+            defaults = {
+                'username': member.name,
+                'user_id': str(member.id),
+                'join_date': member.joined_at
+            }
+
+            discord_user, created = await create_or_get_user(user_id, defaults)
+
+            if created:
+                print(f'Added new user: {discord_user.username}')
+            else:
+                print(f'User {discord_user.username} already exists.')
+
+TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+bot.run(TOKEN)
+
