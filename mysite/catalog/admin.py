01be2685aab68ee371609ce357083dabcf0c852c
bogdan <bogdankibuj@gmail.com>
2025-03-29 21:09:21 +0200

    add admin panel for discordUser model

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DiscordUser
 
-# Register your models here.
+class DiscordUserAdmin(admin.ModelAdmin):
+    list_display = ('username','user_id','join_date')
+    fields = ['username','user_id','join_date']
+admin.site.register(DiscordUser,DiscordUserAdmin)
